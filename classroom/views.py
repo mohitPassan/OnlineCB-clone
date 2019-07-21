5c4da692d4459f0a5d1fe0ab12a222a148e1924d
Mohit Passan <mohitpassan@gmail.com>
2019-07-21 10:55:05 +0530

    Added templates- index.html, to view the database

--- classroom/views.py
+++ classroom/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from classroom import models
 # Create your views here.
+
+def index(request):
+    courses = models.Course.objects.all()
+    Context = {
+        "recommended_courses": courses
+    }
+    return render(request, 'classroom/index.html', Context)
